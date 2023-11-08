f10a645aa89dc8176a595e49103d7a53269bc4cc
NamLH2 <namlh2@vetc.com.vn>
2023-11-08 18:01:54 +0700

    update model

--- internal/model/promotion.go
+++ internal/model/promotion.go
@@ -12,6 +12,10 @@ type Promotion struct {
 	MinTransactionValue float64                   `json:"minTransactionValue" gorm:"default:0"`
 	MaxTransactionValue float64                   `json:"maxTransactionValue" gorm:"null"`
 	MaxUsagePerCustomer int                       `json:"maxUsagePerCustomer" gorm:"null"`
+	Total               int                       `json:"total" gorm:"not null"`
+	RemainingQuantity   int                       `json:"remainingQuantity" `
+	MinDiscountValue    float64                   `json:"minDiscountValue" gorm:"default:0"`
+	MaxDiscountValue    float64                   `json:"maxDiscountValue"`
 
 	EndDate       time.Time    `json:"end_date" gorm:"not null"`
 	DiscountType  DiscountType `json:"discount_type" gorm:"not null;default:FIXED_VALUE"`
