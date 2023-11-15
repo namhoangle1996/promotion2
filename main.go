e62be52ef8309d1f52d3351e64850519a96a1f22
NamLH2 <namlh2@vetc.com.vn>
2023-11-15 18:01:18 +0700

    add infrastructure files

--- main.go
+++ main.go
@@ -1,22 +1,82 @@
 package main
 
-import (
-	"kk/config"
-	"kk/internal/handler/httpHandler"
-	"kk/internal/infrastructure/postgresql"
-	"kk/internal/infrastructure/redis"
-)
+import "fmt"
 
 func main() {
-	conf := config.NewConfig()
-	_ = redis.NewRedisConn(conf.Redis)
+	//conf := config.NewConfig()
+	//_ = redis.NewRedisConn(conf.Redis)
+	//
+	//db := postgresql.NewConn(conf.Postgresql)
+	//
+	////http handler
+	//httpHandler.NewHttpServer(conf.HTTP, db)
+	//
+	//select {}
 
-	db := postgresql.NewConn(conf.Postgresql)
+	fmt.Println("get center points ", findMinHeightTrees(4, [][]int{[]int{1, 2}, []int{1, 3}, []int{0, 1}}))
+}
 
-	//http handler
-	httpHandler.NewHttpServer(conf.HTTP, db)
+func isParadollineNumb() {
+	var rn int
 
-	//kafka consumer handler
+	var input = 142
+	tmp := input
+	for tmp > 0 {
+		rn = rn*10 + tmp%10
+		tmp = tmp / 10
+	}
+	fmt.Println("is palindrome number", isEqualNumbers(input, rn))
+}
 
-	select {}
+func isEqualNumbers(originNum, reverseNum int) bool {
+	return originNum == reverseNum
+}
+
+func findMinHeightTrees(node int, edges [][]int) []int {
+	if node == 1 {
+		return []int{0}
+	}
+
+	queue := []int{}
+	//graph := make(map[int][]int)
+	graph := make([][]int, node)
+	degree := make([]int, node)
+
+	for _, e := range edges {
+		fmt.Println("get edge ", e)
+		na, nb := e[0], e[1]
+		graph[na] = append(graph[na], nb)
+		graph[nb] = append(graph[nb], na)
+		fmt.Println(graph)
+		degree[na]++
+		degree[nb]++
+	}
+
+	for i := range degree {
+		if degree[i] == 1 {
+			queue = append(queue, i)
+		}
+	}
+
+	// topological sort
+	for node > 2 {
+		size := len(queue)
+		node -= size
+
+		for size > 0 {
+			node := queue[0]
+			queue = queue[1:]
+
+			for _, v := range graph[node] {
+				degree[v]--
+				if degree[v] == 1 {
+					queue = append(queue, v)
+				}
+			}
+
+			size--
+		}
+	}
+
+	return queue
 }
